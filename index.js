16fdfe966a8c3298b0b3d4bb901d5ac9cb75084d
ayon121 <borshonsaha1234@gmail.com>
2023-11-07 13:52:20 +0600

    room datails added

--- index.js
+++ index.js
@@ -37,7 +37,14 @@ async function run() {
       res.send(result)
     })
 
-    
+    app.get('/rooms/:id' , async(req , res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const room = await Roomcollections.findOne(query);
+      res.send(room)
+
+    })
+
 
 
     
