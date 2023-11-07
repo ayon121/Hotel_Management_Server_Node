1a65977d559c1c33c0847721a95541e9607c3ab3
ayon121 <borshonsaha1234@gmail.com>
2023-11-07 13:27:48 +0600

    room added

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
-
 const port = process.env.PORT || 5000;
-
 require('dotenv').config()
 // middleware
-app.use(cors())
+
 app.use(express.json())
+app.use(cors())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6rjuyq3.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6rjuyq3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,22 +24,48 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
+
+    const database1 = client.db("RoomsDB");
+    const Roomcollections = database1.collection("Rooms");
+  
+
+    app.get('/rooms', async (req, res) => {
+      const cursor = Roomcollections.find()
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    
+
+
+    
+
+    
+
+   
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Users management server is running')
+  res.send('Brand Shop management server is running')
 })
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`)
 })
+
+// user
+// borshonsaha1234
+// password
+// TLqQE4ymjG3zYyQT
