5eb3e93d95bec2abc2fae3c9855ea74321772b58
ayon121 <borshonsaha1234@gmail.com>
2023-11-07 11:35:12 +0600

    dotenv added

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
-
+require('dotenv').config()
 // middleware
 app.use(cors())
 app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ayon1500:vSezPrmd3pKinn7k@cluster0.6rjuyq3.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6rjuyq3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
