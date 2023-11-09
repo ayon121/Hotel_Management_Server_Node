20569bdec254aad79c1a30a63df9d0924231486e
ayon121 <borshonsaha1234@gmail.com>
2023-11-09 13:01:35 +0600

    jwt added

--- index.js
+++ index.js
@@ -4,10 +4,37 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-// middleware
 
+// middleware
+// token 
+const jwt = require('jsonwebtoken');
+const cookieparser = require('cookie-parser')
 app.use(express.json())
-app.use(cors())
+app.use(cookieparser())
+app.use(cors({
+  origin : ['http://localhost:5173'],
+  credentials : true
+}))
+
+const verifytoken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log('verified' + token);
+  if (!token) {
+    return res.status(401).send({ message: 'forbidden' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    // error
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: 'forbidden' })
+    }
+    // if token is valid then it would be decoded
+    console.log('value in the token', decoded);
+    req.user = decoded
+    next()
+  })
+
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6rjuyq3.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,17 +58,52 @@ async function run() {
     const Roomcollections = database1.collection("Rooms");
     const database2 = client.db("MyBookingDB");
     const Bookingcollections = database2.collection("MyBookings");
-  
+    const database3 = client.db("ReviewDB");
+    const Reviewcollections = database3.collection("MyReview");
+
+
+
+
+
+    // auth related 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user , process.env.ACCESS_TOKEN_SECRET , {expiresIn : '1h'})
+
+      res
+      .cookie('token',token , {
+        httpOnly : true,
+        secure : false,   //set secure false for development purpose
+        
+        // sameSite : 'none',
+
+      })
+
+      .send({success : true})
+    })
+
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log('deleted', user);
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+    })
+
 
     app.get('/rooms', async (req, res) => {
       const cursor = Roomcollections.find()
       const result = await cursor.toArray();
       res.send(result)
     })
+ 
+    app.get('/rooms/:id',  async (req, res) => {
+      // if (req.query.email == req.user.email) {
+      //   return res.status(403).send({ message: 'forbidden' })
 
-    app.get('/rooms/:id' , async(req , res) => {
+      // }
+      
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const room = await Roomcollections.findOne(query);
       res.send(room)
 
@@ -62,20 +124,58 @@ async function run() {
 
 
 
-    app.get('/mybookings/:id', async (req, res) => {
-      const id = req.params.id;
+    app.get('/mybookings/:id',verifytoken, async (req, res) => {
+      if(req.params?.id !== req.user.email){
+        return res.status(403).send({message : 'forbidden'})
+        
+    }
+
+      
+
+      const id = req.params?.id;
       console.log(id);
 
-      const query = {useremail: id };
+      const query = { useremail: id };
       const bookings = await Bookingcollections.find(query).toArray();
       res.send(bookings)
 
     })
 
-    
+    app.get('/reviews/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
 
-   
-   
+      const query = { id: id };
+      const Review = await Reviewcollections.find(query).toArray();
+      res.send(Review)
+
+    })
+    app.get('/reviews', async (req, res) => {
+      const cursor = Reviewcollections.find()
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/reviews', async (req, res) => {
+      const Review = req.body;
+      const result = await Reviewcollections.insertOne(Review);
+      res.send(result)
+
+    })
+
+    app.delete('/mybookings/:id', async (req, res) => {
+      const id = req.params.id
+      console.log('plz delete  from database', id);
+      const query = { _id: new ObjectId(id) }
+      // const result = await Bookingcollections.deleteOne(query)
+      // res.send(result)
+    })
+    9
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
